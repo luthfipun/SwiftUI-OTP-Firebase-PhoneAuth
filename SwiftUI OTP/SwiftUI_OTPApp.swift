9a9e6ed931551b9e7c666d960aaef50e7ed02068
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-03-07 22:34:13 +0700

    Setup and Configure Firebase

--- SwiftUI OTP/SwiftUI_OTPApp.swift
+++ SwiftUI OTP/SwiftUI_OTPApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftUI_OTPApp: App {
@@ -16,5 +17,17 @@ struct SwiftUI_OTPApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDeletegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
     }
 }
